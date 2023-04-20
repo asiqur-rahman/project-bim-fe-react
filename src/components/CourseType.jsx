a2f59bcc30dc2bb6c90f1574d15796935fb48873
Md. Asiqur Rahman Khan <asiqur.rahman.khan.0@gmail.com>
2023-04-20 15:55:59 +0600

    Update

--- src/components/CourseType.jsx
+++ src/components/CourseType.jsx
@@ -3,38 +3,36 @@ import Axios, {web as AxiosWeb} from '../helper/axios'
 import { Link } from "react-router-dom";
 
 function Course(props) {
-     
+
+    const [courseDetails, setCourseDetails]= useState(false);
     const RenderCourse = () =>{
 
         useEffect(()=>{
-            if(props.id){
+            if(props.id && !courseDetails){
               AxiosWeb.get(`/courseType/details/${props.id}`)
               .then(result=>{
-                  if(result.data.status==200){
-                    window.SpinnerHide();
-                    // setPageDetails(result.data.data)
-                  }
+                    if(result.data.status==200){
+                        setCourseDetails(result.data.data);
+                        window.SpinnerHide();
+                    }
               })
             }
-            
-          },[props.id])
-
+        },[props.id])
+        
         useEffect(() => {
             AOS.init();
-            // window.SpinnerHide();
-
         }, []);
-        
+
         return (
             <>
-            {props.id && <>
+            {courseDetails && <>
                 <div className="page-banner-area bg-1">
                     <div className="container">
                         <div className="page-banner-content">
-                            <h1>Training Courses</h1>
+                            <h1>{courseDetails.courseTypeName}</h1>
                             <ul>
                                 <li><a href="#">Study</a></li>
-                                <li>Training Courses</li>
+                                <li>{courseDetails.courseTypeName}</li>
                             </ul>
                         </div>
                     </div>
@@ -43,57 +41,20 @@ function Course(props) {
                     <div className="container">
                         <div className="row justify-content-center">
 
-                            <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200" data-aos-once="true">
-                                <div className="single-courses-card">
-                                    <div className="courses-img">
-                                        <Link to={`/course-details/1`}><img loading="lazy" src="/images/courses/courses-1.jpg" alt="Image" /></Link>
+                            {courseDetails.courses.map((item,i)=>{
+                                return (
+                                    <div key={i} className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200" data-aos-once="true">
+                                        <div className="single-courses-card">
+                                            <div className="courses-img">
+                                                <Link to={`/course-details/${item.id}`}><img loading="lazy" src={item.files[0].link} alt="Image" /></Link>
+                                            </div>
+                                            <div className="courses-content">
+                                                <Link to={`/course-details/${item.id}`}><h3>{item.courseName}<br /><small style={{fontSize:"15px"}}>Code – ‘{item.courseCode}’</small></h3></Link>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="courses-content">
-                                        <Link to={`/course-details/1`}><h3>Training Courses</h3></Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200" data-aos-once="true">
-                                <div className="single-courses-card">
-                                    <div className="courses-img">
-                                        <Link to={`/course-details/1`}><img loading="lazy" src="/images/courses/courses-1.jpg" alt="Image" /></Link>
-                                    </div>
-                                    <div className="courses-content">
-                                        <Link to={`/course-details/1`}><h3>Certificate Courses</h3></Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200" data-aos-once="true">
-                                <div className="single-courses-card">
-                                    <div className="courses-img">
-                                        <Link to={`/course-details/1`}><img loading="lazy" src="/images/courses/courses-1.jpg" alt="Image" /></Link>
-                                    </div>
-                                    <div className="courses-content">
-                                        <Link to={`/course-details/1`}><h3>Diploma Courses</h3></Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200" data-aos-once="true">
-                                <div className="single-courses-card">
-                                    <div className="courses-img">
-                                        <Link to={`/course-details/1`}><img loading="lazy" src="/images/courses/courses-1.jpg" alt="Image" /></Link>
-                                    </div>
-                                    <div className="courses-content">
-                                        <Link to={`/course-details/1`}><h3>PGD Courses</h3></Link>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200" data-aos-once="true">
-                                <div className="single-courses-card">
-                                    <div className="courses-img">
-                                        <Link to={`/course-details/1`}><img loading="lazy" src="/images/courses/courses-1.jpg" alt="Image" /></Link>
-                                    </div>
-                                    <div className="courses-content">
-                                        <Link to={`/course-details/1`}><h3>Research Courses</h3></Link>
-                                    </div>
-                                </div>
-                            </div>
+                                )
+                            })}
 
                         </div>
                     </div>
