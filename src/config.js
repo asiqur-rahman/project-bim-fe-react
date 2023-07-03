1375c45b08e816c0e3c67735da6560c668b20cbc
Asiqur Rahman Khan <asiqur.rahman@datasoft-bd.com>
2023-07-03 12:34:57 +0600

    Update DB

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 const Config = {
-    MODE: "development",
+    MODE: "production",
     applicationInfo:{
       appName: "Sus-Plan",
       devOrgName: "Bangladesh Institute of Management (BIM)",
       devOrgLink: "http://www.bim.gov.bd/"
     },
     applicationSettings:{
-      developmentBaseURL : "http://localhost:3334/api",//"https://braintechsolution.com:3335/api",//"http://localhost:3334/api",//
-      productionBaseURL : "https://braintechsolution.com:3331/api",
+      developmentBaseURL : "http://localhost:3334/api",//"http://localhost:3334/api",//"https://braintechsolution.com:3335/api",//"http://localhost:3334/api",//
+      productionBaseURL : "https://api.sustainability.com.bd/api",
       portalURL : "http://www.admin.sustainability.com.bd/",
       SESSION_NAME : "Sus-Plan",
       BASE_TOKEN : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbiIsImZ1bGxfbmFtZSI6IkFzaXF1ciBSYWhtYW4gS2hhbiIsInVzZXJfaWQiOiIxIiwicm9sZV9pZCI6IjEiLCJyb2xlX2NvZGUiOiIxIiwicm9sZV9uYW1lIjoiQWRtaW4iLCJjbGllbnRJcCI6Ijo6MSIsImlhdCI6MTY4MTUzNDIxMH0.eq79fG5j1UKnG84QgmHfj3ad9Jnbj7n3FmfvfL6Xue4"
